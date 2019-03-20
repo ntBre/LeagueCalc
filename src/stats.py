5a7b60fcb681fa3d94021a61811df1d6724ebf81
Brent Westbrook <bwestbro@stedwards.edu>
2019-03-20 12:44:05 -0500

    update level_up_stats

--- src/stats.py
+++ src/stats.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 import json
+import subprocess
 
 # call calculation
 #TODO post level runes items champ
@@ -10,20 +11,12 @@ def stats():
     level = 13
     runes = {}
     items = {}
-    
-    champ = json.loads(open("../champions/Aatrox.json").read())
+    champ = json.loads(open("./champions/Aatrox.json").read())
     champ = level_up_stats(champ, level)
     return json.dumps(champ)
 
 def level_up_stats(input_champ, level):
-    output_champ = {}
-    # stats that level up
-    level_stats = ["armor", "hp", "attackdamage", "mp", "spellblock", "mpregen", "hpregen", "attackspeed"]
-    if attribute in level_stats:
-        for attribute in input_champ.keys(): 
-            output_champ[attribute] = input_champ[attribute] + (input_champ[dict[attribute]]*level)
-            
-{
+    output_champ = {
     "armor":"armorperlevel", 
     "attackdamage":"attackdamageperlevel",
     "hp":"hpperlevel",
@@ -33,27 +26,16 @@ def level_up_stats(input_champ, level):
     "mpregen":"mpregenperlevel", 
     "movespeed":None,
     "crit":None,
-    "attackspeed":}
+    "attackspeed":None
+    } 
+    # stats that level up
+    level_stats = ["armor", "hp", "attackdamage", "mp", "spellblock", "mpregen", "hpregen", "attackspeed"]
+    for attribute in input_champ.keys(): 
+        if attribute in level_stats:
+            scaling = attribute + "perlevel"
+            output_champ[attribute] = input_champ[attribute] + (input_champ[scaling]*level)
+        else:
+            output_champ[attribute] = input_champ[attribute]
 
-    "armorperlevel": 3.25,
-    "hpperlevel": 80,
-    "attackdamage": 60,
-    "mpperlevel": 0,
-    "spellblockperlevel": 1.25,
-    "armor": 33,
-    "hp": 580,
-    "hpregenperlevel": 0.75,
-    "spellblock": 32.1,
-    "attackrange": 175,
-    "movespeed": 345,
-    "attackdamageperlevel": 5,
-    "mpregenperlevel": 0,
-    "mp": 0,
-    "attackspeed": 0.651,
-    "crit": 0,
-    "mpregen": 0,
-    "attackspeedperlevel": 2.5,
-    "hpregen": 8,
-    "critperlevel": 0
-
-    return output_champ["armor"] 
+    return output_champ
+            
